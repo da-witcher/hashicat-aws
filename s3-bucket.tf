63b69f1dd78951fcaef0b9c9dae8498e8e313a90
Aasshu <aasshusethi@shaw.ca>
2022-01-18 14:18:01 -0700

    Ash added s3 bucket / change name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "${var.prefix}-s3-bucket"
+  bucket = "${var.prefix}-01182022-s3-bucket"
   acl    = "private"
 
   versioning = {
